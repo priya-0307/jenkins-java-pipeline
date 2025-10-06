99e5a1e9ed6e72c33aad04e435eb5da6016936ef
Priyadharshini <priyamathiyalakan03@gmail.com>
2025-10-06 15:04:40 +0530

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+package test.java;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testAddNumbers() {
+        int result = main.java.App.addNumbers(5, 7);
+        assertEquals(12, result);
+    }
+}
