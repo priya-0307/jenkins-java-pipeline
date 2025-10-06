d1bdc62581f298dcaf1b73d605b47c1014568506
Priyadharshini <priyamathiyalakan03@gmail.com>
2025-10-06 15:02:13 +0530

    Create App.java

---
+++ src/java/App.java
@@ -0,0 +1,12 @@
+package main.java;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins CI/CD Pipeline with Java!");
+    }
+
+    // A simple method to test
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
